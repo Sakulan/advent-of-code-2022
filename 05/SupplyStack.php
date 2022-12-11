8aea1d0489d3cca65acaed84e9cecfdf97d962d2
Siufong <siufong.h@gmail.com>
2022-12-11 20:58:50 +0100

    Beginning of day 5

---
+++ 05/SupplyStack.php
@@ -0,0 +1,19 @@
+<?php
+
+class SupplyStack
+{
+    public function supplyStackCrate($input)
+    {
+        //modify the input and create arrays
+        $one = ['N', 'W', 'B'];
+        $two = ['B', 'M', 'D', 'T', 'P'];
+        $three = ['N', 'W', 'B'];
+        $four = ['N', 'W', 'B'];
+        $five = ['N', 'W', 'B'];
+        $six = ['N', 'W', 'B'];
+        $seven = ['N', 'W', 'B'];
+        $eight = ['N', 'W', 'B'];
+        $nine = ['N', 'W', 'B'];
+    }
+
+}
